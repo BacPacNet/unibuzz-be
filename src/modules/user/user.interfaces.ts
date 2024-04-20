fa6a9e6f3e414c3e3a09042b167722d8fd0ef8a7
pratikliftoff <pratik@liftoffllc.com>
2024-04-20 17:19:41 +0530

    fix: boiler-plate setup

---
+++ src/modules/user/user.interfaces.ts
@@ -0,0 +1,35 @@
+import mongoose, { Model, Document } from 'mongoose';
+import { QueryResult } from '../paginate/paginate';
+import { AccessAndRefreshTokens } from '../token/token.interfaces';
+
+export interface IUser {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  gender: string;
+  dob: string;
+  role: string;
+  isEmailVerified: boolean;
+  createdAt: Date | string;
+}
+
+export interface IUserDoc extends IUser, Document {
+  isPasswordMatch(password: string): Promise<boolean>;
+}
+
+export interface IUserModel extends Model<IUserDoc> {
+  isEmailTaken(email: string, excludeUserId?: mongoose.Types.ObjectId): Promise<boolean>;
+  paginate(filter: Record<string, any>, options: Record<string, any>): Promise<QueryResult>;
+}
+
+export type UpdateUserBody = Partial<IUser>;
+
+export type NewRegisteredUser = Omit<IUser, 'role' | 'isEmailVerified'>;
+
+export type NewCreatedUser = Omit<IUser, 'isEmailVerified'>;
+
+export interface IUserWithTokens {
+  user: IUserDoc;
+  tokens: AccessAndRefreshTokens;
+}
