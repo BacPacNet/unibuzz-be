166ede075fe38287b5d98582addff5c3ad99f087
Pratik Yadav <pratik@liftoffllc.com>
2024-09-24 13:15:29 +0530

    update env added smtp server for otp

--- src/modules/loginEmailVerification/loginEmailVerification.service.ts
+++ src/modules/loginEmailVerification/loginEmailVerification.service.ts
@@ -1,6 +1,8 @@
 import httpStatus from 'http-status';
 import { ApiError } from '../errors';
 import loginEmailVerificationModal from './loginEmailVerification.modal';
+import 'dotenv/config';
+import { sendEmail } from '../email/email.service';
 
 export const createloginEmailVerificationOtp = async (email: string) => {
   const data = {
@@ -17,6 +19,21 @@ export const createloginEmailVerificationOtp = async (email: string) => {
     const updatedData = { ...data };
     await loginEmailVerificationModal.updateOne({ email }, updatedData);
   }
+
+  await sendEmail(
+    email,
+    'Verify Your Email Address - OTP Code Inside',
+    '',
+    `
+    <p>Thank you for registering with <strong>Unibuzz</strong>. To complete your email verification, please enter the following One-Time Password (OTP) on the verification page:</p>
+    <h2>${data.otp}</h2>
+    <p>This OTP is valid for the next <strong>5 minutes</strong>. Please do not share this code with anyone for security reasons.</p>
+    <p>If you did not request this verification, please ignore this email.</p>
+    <br>
+    <p>Best regards,<br><strong>Unibuzz</strong> Support Team</p>
+    <p>Contact: info@unibuzz.org</p>
+  `
+  );
 };
 
 export const checkloginEmailVerificationOtp = async (otp: string, email: string) => {
