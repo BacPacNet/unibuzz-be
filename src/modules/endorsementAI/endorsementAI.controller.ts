c113976c972cdf0c0ed1fbf8d5cdd41ecbe1a484
coolpinkzz <masterpratikyadav@gmail.com>
2025-01-11 12:23:59 +0530

    added endorseai api

---
+++ src/modules/endorsementAI/endorsementAI.controller.ts
@@ -0,0 +1,37 @@
+import { Response } from 'express';
+import { userIdExtend } from 'src/config/userIDType';
+import EndorseAIModel from './endorsementAI.model';
+
+export const CreateEndorseAI = async (req: userIdExtend, res: Response) => {
+  try {
+    const { communityId } = req.body;
+    const endorsement = new EndorseAIModel({
+      userId: req.userId,
+      communityId,
+    });
+    const savedEndorsement = await endorsement.save();
+    res.status(201).json(savedEndorsement);
+  } catch (error: any) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const GetEndorseAIByCommunityId = async (req: userIdExtend, res: Response) => {
+  try {
+    const { communityId } = req.params;
+    const endorsements = await EndorseAIModel.find({ communityId }).select('totalGoal numberOfUsersEndorsed');
+
+    const totalGoal = 100;
+    const totalUsersEndorsed = endorsements.length;
+
+    const percentage = totalGoal ? (totalUsersEndorsed / totalGoal) * 100 : 0; // Handle division by zero
+
+    res.status(200).json({
+      totalGoal,
+      totalUsersEndorsed,
+      percentage: percentage.toFixed(0),
+    });
+  } catch (error: any) {
+    res.status(500).json({ error: error.message });
+  }
+};
