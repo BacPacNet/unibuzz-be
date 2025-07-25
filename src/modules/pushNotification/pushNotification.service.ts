b73bd97ff4122dc30c5402dd77a0131179ef9c7a
Aamil13 <aamil.shafi13@gmail.com>
2025-07-25 12:41:15 +0530

    removed throw from send push notification if err

--- src/modules/pushNotification/pushNotification.service.ts
+++ src/modules/pushNotification/pushNotification.service.ts
@@ -19,12 +19,10 @@ export const createPushNotification = async (userId: string, token: string) => {
   return pushNotification;
 };
 
-export const sendPushNotification = async (userId: string, title: string, body: string,  data:any = {}) => {
+export const sendPushNotification = async (userId: string, title: string, body: string, data: any = {}) => {
   const pushNotification = await pushNotificationModal.findOne({ user_Id: userId });
 
-  if (!pushNotification) throw new ApiError(httpStatus.NOT_FOUND, 'notification not found!');
-
-
+  if (!pushNotification) return;
 
   const message = {
     notification: {
@@ -39,140 +37,133 @@ export const sendPushNotification = async (userId: string, title: string, body: 
   return response;
 };
 
-
 export const sendMessagePushNotification = async (
-    userId: string,
-    title: string,
-    body: string,
-    data: any = {},
-    channelId: string = 'message_channel' 
-  ) => {
-    const pushNotification = await pushNotificationModal.findOne({ user_Id: userId });
-  
-    if (!pushNotification) {
-      throw new ApiError(httpStatus.NOT_FOUND, 'notification not found!');
-    }
-  
-    const message = {
+  userId: string,
+  title: string,
+  body: string,
+  data: any = {},
+  channelId: string = 'message_channel'
+) => {
+  const pushNotification = await pushNotificationModal.findOne({ user_Id: userId });
+
+  if (!pushNotification) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'notification not found!');
+  }
+
+  const message = {
+    notification: {
+      title: title || 'Notification',
+      body: body || 'You have a new message',
+    },
+    data: {
+      ...data,
+    },
+    token: pushNotification.token,
+    android: {
       notification: {
-        title: title || 'Notification',
-        body: body || 'You have a new message',
+        channelId,
+        tag: 'msg_tag',
+        priority: 'high' as const,
+      },
+    },
+  };
+
+  const response = await admin.messaging().send(message);
+  return response;
+};
+
+export const sendTestPushNotifications = async (userId: string) => {
+  const pushNotification = await pushNotificationModal.findOne({ user_Id: userId });
+
+  if (!pushNotification) throw new ApiError(httpStatus.NOT_FOUND, 'notification not found!');
+
+  const token = pushNotification.token;
+
+  const messages = [
+    {
+      notification: {
+        title: 'Default Notification',
+        body: 'This is a simple notification',
+      },
+      data: { type: 'DEFAULT' },
+      token,
+    },
+    {
+      notification: {
+        title: 'With Custom Data',
+        body: 'Tap to see post',
       },
       data: {
-        ...data,
+        type: 'POST',
+        postId: '123456',
+        customInfo: 'extra data',
       },
-      token: pushNotification.token,
+      token,
+    },
+    {
+      data: {
+        type: 'SILENT',
+        info: 'No notification field, app handles this silently',
+      },
+      token,
+      android: {
+        priority: 'high',
+      },
+    },
+    {
+      notification: {
+        title: 'Click Action Test',
+        body: 'Tap me to open app screen',
+      },
+      data: {
+        type: 'NAVIGATION',
+        screen: 'SinglePost',
+        postId: 'post789',
+      },
+      token,
+    },
+    {
+      notification: {
+        title: 'Big Picture Notification',
+        body: 'This has an image!',
+        imageUrl:
+          'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%2Fid%2FOIP.39u8AR1NJ-P7gkXAnsN9bwHaEF%3Fpid%3DApi&f=1&ipt=cf2e5b4f1a90f8c43aea4b90814cd7df26fd631d60dc3c819218574bb5bfa197&ipo=images',
+      },
+      data: {
+        type: 'IMAGE',
+      },
+      token,
+    },
+    {
+      notification: {
+        title: 'Grouped Notification',
+        body: 'This is part of a group',
+      },
+      data: {
+        type: 'GROUP',
+        group: 'group_123',
+      },
       android: {
         notification: {
-          channelId,      
-          tag: 'msg_tag', 
-          priority: 'high' as const,
+          tag: 'group_tag',
         },
       },
-    };
-  
-    const response = await admin.messaging().send(message);
-    return response;
-  };
+      token,
+    },
+    {
+      notification: {
+        title: 'Message Notification',
+        body: 'You got a new message',
+      },
+      data: {
+        type: 'MESSAGE',
+        sender_id: 'user123',
+      },
+      token,
+    },
+  ];
 
+  const responses = await Promise.all(messages.map((msg) => admin.messaging().send(msg as any)));
 
-
-
-
-  export const sendTestPushNotifications = async (userId: string) => {
-    const pushNotification = await pushNotificationModal.findOne({ user_Id: userId });
-  
-    if (!pushNotification) throw new ApiError(httpStatus.NOT_FOUND, 'notification not found!');
-  
-    const token = pushNotification.token;
-  
-    const messages = [
-      {
-        notification: {
-          title: "Default Notification",
-          body: "This is a simple notification",
-        },
-        data: { type: "DEFAULT" },
-        token,
-      },
-      {
-        notification: {
-          title: "With Custom Data",
-          body: "Tap to see post",
-        },
-        data: {
-          type: "POST",
-          postId: "123456",
-          customInfo: "extra data",
-        },
-        token,
-      },
-      {
-        data: {
-          type: "SILENT",
-          info: "No notification field, app handles this silently",
-        },
-        token,
-        android: {
-          priority: "high",
-        },
-      },
-      {
-        notification: {
-          title: "Click Action Test",
-          body: "Tap me to open app screen",
-        },
-        data: {
-          type: "NAVIGATION",
-          screen: "SinglePost",
-          postId: "post789",
-        },
-        token,
-      },
-      {
-        notification: {
-          title: "Big Picture Notification",
-          body: "This has an image!",
-          imageUrl: "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%2Fid%2FOIP.39u8AR1NJ-P7gkXAnsN9bwHaEF%3Fpid%3DApi&f=1&ipt=cf2e5b4f1a90f8c43aea4b90814cd7df26fd631d60dc3c819218574bb5bfa197&ipo=images",
-        },
-        data: {
-          type: "IMAGE",
-        },
-        token,
-      },
-      {
-        notification: {
-          title: "Grouped Notification",
-          body: "This is part of a group",
-        },
-        data: {
-          type: "GROUP",
-          group: "group_123",
-        },
-        android: {
-          notification: {
-            tag: "group_tag",
-          },
-        },
-        token,
-      },
-      {
-        notification: {
-          title: "Message Notification",
-          body: "You got a new message",
-        },
-        data: {
-          type: "MESSAGE",
-          sender_id: "user123",
-        },
-        token,
-      },
-    ];
-  
-    const responses = await Promise.all(
-      messages.map((msg) => admin.messaging().send(msg as any))
-    );
-  
-    return responses;
-  };
-  
+  return responses;
+};
