b7cd0862a407e4a244c275268209a3d6c7296ebc
Aamil13 <aamil.shafi13@gmail.com>
2026-01-21 18:26:44 +0530

    community group title update on update group fix

--- src/modules/communityGroup/communityGroup.service.ts
+++ src/modules/communityGroup/communityGroup.service.ts
@@ -69,6 +69,9 @@ export const updateCommunityGroup = async (id: mongoose.Types.ObjectId, body: an
   if (communityGroupAccess) {
     communityGroup.communityGroupAccess = communityGroupAccess;
   }
+  if (title !== undefined) {
+    communityGroup.title = title;
+  }
 
   Object.assign(communityGroup, restBody);
 
